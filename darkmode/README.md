1ccd7ef7f31ff5fefa3f3fb6e11ece4e6a148eb8
Rivit Not Vivit <jack.problems@protonmail.com>
2024-09-24 12:58:26 -0700

    Deployed on https://skins-charts-darkmode.onrender.com

--- darkmode/README.md
+++ darkmode/README.md
@@ -1,3 +1,5 @@
+# deployed on
+https://skins-charts-darkmode.onrender.com
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- darkmode/src/index.js
+++ darkmode/src/index.js
@@ -1,65 +1,28 @@
-import React, { useState,useEffect} from "react";
-import {createRoot} from 'react-dom/client';
-import ReactDOM from "react-dom";
-// import axios from "axios";
+import React, { useState, useEffect } from "react";
+import {createRoot} from "react-dom";
+import axios from "axios";
 import {useDarkMode} from './hooks/useDarkMode'
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
-// import {useHttp} from './hooks/useHttp'
+
 import "./styles.scss";
-const config = {
-  method: "GET",
-  // mode:"no-cors",
-  headers: {
-    "Access-Control-Allow-Origin" : "http://localhost:3000",
-    "Access-Control-Allow":"*",
-    "Content-type": "application/json; charset=UTF-8"
-  }};
+
 const App = () => {
   const [coinData, setCoinData] = useState([]);
   const [darkMode, setDarkMode] = useDarkMode('darkMode');
 
-// const {data:loadedData,loading,error} = useHttp("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true",config,[])
   useEffect(() => {
-
-      async function getFetch()  {
-      try{
-        const req = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true",config)
-      const jsn = req.json();
-      setCoinData(jsn);
-      }catch(e){
-        console.log(e);
-      }
-      
-      }
-      getFetch();
-
-    // axios
-    //   .get(
-    //     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-    //   )
-    //   .then(res => setCoinData(res.data))
-    //   .catch(err => console.log(err));
-  }, [coinData]);
-  // if(loading){
-  //   return(
-  //     <p>Loading up please wait</p>
-  //   )
-  // }
-  // if(!loading){
-  //   setCoinData(loadedData);
-  // }
-  // if(error){
-  //   return (
-  //     <p>
-  //       Theres an error {error}
-  //     </p>
-  //   )
-  // }
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+      )
+      .then(res => setCoinData(res.data))
+      .catch(err => console.log(err));
+  }, []);
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-     {coinData.length &&  <Charts coinData={coinData} />}
+      <Charts coinData={coinData} />
     </div>
   );
 };
